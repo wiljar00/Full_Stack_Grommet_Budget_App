d85d34a4702c36c1a343a6089426c39fefaf5173
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-01-28 01:52:55 -0700

    Add multiple cards as page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Button, Grommet, Header, Main, PageContent, PageHeader, Text, grommet } from 'grommet';
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Grommet, Header, Heading, Main, PageContent, PageHeader, Paragraph, Text, grommet } from 'grommet';
 import { Moon, Sun } from 'grommet-icons';
 import { deepMerge } from 'grommet/utils';
 
@@ -27,6 +27,35 @@ const AppBar = (props : any) => (
     {...props}
   />
 )
+
+// Notes on the typescript error:
+// For an object, you need to declare the type as:
+// {text} : {text:any}
+// For objects with more props:
+// {a,b} : {a:any, b:any}
+
+const CardTemplate = ({ title } : { title:any }) => {
+  return (
+    <Card>
+      <CardHeader pad="medium">
+        <Heading level={2} margin="none">
+          {title}
+        </Heading>
+      </CardHeader>
+      <CardBody pad="medium">
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          porttitor non nulla ac vehicula. Aliquam erat volutpat. Mauris auctor
+          faucibus est at mattis. Aliquam a enim ac nisi aliquam consectetur et
+          ac velit. Mauris ut imperdiet libero.
+        </Paragraph>
+      </CardBody>
+      <CardFooter pad="medium" background="background-contrast">
+        Footer
+      </CardFooter>
+    </Card>
+  );
+};
 
 function App() {
   const [dark, setDark] = useState(false);
@@ -56,6 +85,9 @@ function App() {
         </AppBar>
         <PageContent>
           <PageHeader title="Welcome to Grommet!"/>
+          <CardTemplate title={"Card 1"} />
+          <CardTemplate title={"Card 2"}/>
+          <CardTemplate title={"Card 3"}/>
         </PageContent>
       </Main>
     </Grommet>
