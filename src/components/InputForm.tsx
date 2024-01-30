d40109891bb1672a29951b1882742df15263d914
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-01-29 23:55:24 -0700

    Add form to add amounts to a running total when button is clicked

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,23 +1,46 @@
-import React from "react";
-import { Box, DateInput, TextInput } from "grommet";
+import React, { useState } from "react";
+import { Box, Button, Card, CardBody, DateInput, Heading, TextInput } from "grommet";
+
+
 
 const InputForm = () => {
     const [value, setValue] = React.useState('');
+    const [amounts, setAmount] = useState<number[]>([]);
+
+    const addAmount = () => {
+        const amount = parseFloat(value);
+        if (!isNaN(amount)) {
+            setAmount(prevAmounts => [...prevAmounts, amount]);
+            setValue('');
+        }
+    } 
+
+    const total = amounts.reduce((acc, curr) => acc + curr, 0)
 
     return (
         <div>
-            <Box direction="row">
-                <TextInput
-                    placeholder="Enter an amount"
-                    value={value}
-                    onChange={event => setValue(event.target.value)}
-               />
-
-                <DateInput
-                    format="mm/dd/yyyy"
-                    value={(new Date()).toISOString()}
-                    onChange={({ value }) => {}}
-                />
+            <Box direction="column">
+                <Heading level={2} alignSelf="center">Entries</Heading>
+                <br />
+                <Card width="medium" align="center" alignSelf='center' background="light-1" margin="small">
+                    <CardBody pad="medium">{ `$ ${total.toFixed(2)}` }</CardBody>
+                </Card>
+                <Box direction="row">
+                    <TextInput
+                        placeholder="Enter an amount"
+                        value={value}
+                        onChange={event => setValue(event.target.value)}
+                    />
+                    <DateInput
+                        format="mm/dd/yyyy"
+                        value={(new Date()).toISOString()}
+                        onChange={({ value }) => {}}
+                    />
+                    <Button primary label="Add"
+                         
+                        onClick={addAmount}
+                    />
+                </Box>
             </Box>
         </div>
     );
