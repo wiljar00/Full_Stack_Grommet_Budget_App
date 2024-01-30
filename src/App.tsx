bd3394d0d3d7a7347b9603fda2140e4f21ead866
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-01-29 23:08:45 -0700

    Add basic wireframe functionality and tabs for each section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Button, Grommet, Header, Main, PageContent, PageHeader, Tab, Tabs, Text, grommet } from 'grommet';
+import { Box, Button, Grommet, Header, Main, PageContent, Tab, Tabs, Text, grommet } from 'grommet';
 import { Moon, Sun } from 'grommet-icons';
 import { deepMerge } from 'grommet/utils';
 import SampleCard from './components/SampleCard';
@@ -57,20 +57,75 @@ function App() {
           />
         </AppBar>
         <PageContent>
-          <PageHeader title="Welcome to Grommet!"/>
+          <br />
+          <Tabs>
+          <Tab title="Expenses">
+              <Box pad="medium">
+                <Text alignSelf='center'>
+                  Add expenses. 
+                  TODO: App should have a box/form for amount and date.
+                  Add total to page and app.
+                  When saving, update totals.
+                </Text>
+                <Text>
+                  expense     date
+                </Text>
+                <Text>
+                  expense     date
+                </Text>
+              </Box>
+            </Tab>
+            <Tab title="Income">
+              <Box pad="medium">
+                <Text alignSelf='center'>
+                  Add incomes. App should have a box for amount and date and totals
+                </Text>
+                <Text>
+                  income     date
+                </Text>
+                <Text>
+                  income     date
+                </Text>
+              </Box>
+            </Tab>
+            <Tab title="Combined view">
+              <Box pad="medium">
+                <Text alignSelf='center'>
+                  This will show all expenses combined by months ordered by date. 
+                </Text>
+                <Tabs>
+                  <Tab title="January" >
+                    <Box pad="medium">
+                      <Text>
+                        expense     date
+                      </Text>
+                      <Text>
+                        income     date
+                      </Text>
+                      <Text>
+                        expense...
+                      </Text>
+                      <Text>
+                        income     date
+                      </Text>
+                    </Box>
+                  </Tab>
+                </Tabs>
+              </Box>
+            </Tab>
+            <Tab title="Budget Summary">
+              <Box pad="medium">
+                <BudgetTable />
+              </Box>
+            </Tab>
 
-            <Tabs>
-              <Tab title="Expenses">
-                <Box pad="medium">
-                  <SampleCard />
-                </Box>
-              </Tab>
-              <Tab title="Budget Summary">
-                <Box pad="medium">
-                  <BudgetTable />
-                </Box>
-              </Tab>
-            </Tabs>
+            <Tab title="Card Samples">
+              <Box pad="medium">
+                <SampleCard />
+              </Box>
+            </Tab>
+
+          </Tabs>
 
         </PageContent>
       </Main>
