db23ef2da2e73ed93c5eb10df2f21ddbbbb7c559
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-01-29 23:32:34 -0700

    Update InputForm.tsx

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,13 +1,24 @@
-import { Box, Text } from "grommet";
+import React from "react";
+import { Box, DateInput, TextInput } from "grommet";
 
 const InputForm = () => {
+    const [value, setValue] = React.useState('');
+
     return (
         <div>
-            <Box>
-                <Text>
-                  testing....
-                </Text>
-              </Box>
+            <Box direction="row">
+                <TextInput
+                    placeholder="Enter an amount"
+                    value={value}
+                    onChange={event => setValue(event.target.value)}
+               />
+
+                <DateInput
+                    format="mm/dd/yyyy"
+                    value={(new Date()).toISOString()}
+                    onChange={({ value }) => {}}
+                />
+            </Box>
         </div>
     );
 }
