02f86c643e1c4902cc277fc183e185de61dc771a
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-01-29 22:45:24 -0700

    Update BudgetTable.tsx

--- src/components/BudgetTable.tsx
+++ src/components/BudgetTable.tsx
@@ -1,7 +1,36 @@
+import { Box, DataTable, Meter, Text } from "grommet";
+
 const BudgetTable = () => {
     return (
         <div>
-
+            <DataTable
+                columns={[
+                {
+                    property: 'name',
+                    header: <Text>Name</Text>,
+                    primary: true,
+                },
+                {
+                    property: 'percent',
+                    header: 'Complete',
+                    render: datum => (
+                    <Box pad={{ vertical: 'xsmall' }}>
+                        <Meter
+                        values={[{ value: datum.percent }]}
+                        thickness="small"
+                        size="small"
+                        />
+                    </Box>
+                    ),
+                },
+                ]}
+                data={[
+                    { name: 'Alan', percent: 20 },
+                    { name: 'Bryan', percent: 30 },
+                    { name: 'Chris', percent: 40 },
+                    { name: 'Eric', percent: 80 },
+                ]}
+            />
         </div>
     );
 }
