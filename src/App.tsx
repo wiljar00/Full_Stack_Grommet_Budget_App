aff2e1e29825f7b5d7c64a00e7e059af5fb4c65b
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-01-29 22:37:31 -0700

    Pull cards out to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Grommet, Header, Heading, Main, PageContent, PageHeader, Paragraph, ResponsiveContext, Tab, Tabs, Text, grommet } from 'grommet';
+import { Box, Button, Grommet, Header, Main, PageContent, PageHeader, Tab, Tabs, Text, grommet } from 'grommet';
 import { Moon, Sun } from 'grommet-icons';
 import { deepMerge } from 'grommet/utils';
 
@@ -27,37 +27,6 @@ const AppBar = (props : any) => (
     {...props}
   />
 )
-
-// Notes on the typescript error:
-// For an object, you need to declare the type as:
-// {text} : {text:any}
-// For objects with more props:
-// {a,b} : {a:any, b:any}
-
-const CardTemplate = ({ title } : { title:any }) => {
-  const size = useContext(ResponsiveContext);
-
-  return (
-    <Card>
-      <CardHeader pad="medium">
-        <Heading level={2} margin="none">
-          {title}
-        </Heading>
-      </CardHeader>
-      <CardBody pad="medium">
-        <Paragraph maxLines={size === "small" ? 3 : undefined}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          porttitor non nulla ac vehicula. Aliquam erat volutpat. Mauris auctor
-          faucibus est at mattis. Aliquam a enim ac nisi aliquam consectetur et
-          ac velit. Mauris ut imperdiet libero.
-        </Paragraph>
-      </CardBody>
-      <CardFooter pad="medium" background="background-contrast">
-        Footer
-      </CardFooter>
-    </Card>
-  );
-};
 
 function App() {
   const [dark, setDark] = useState(false);
@@ -97,9 +66,7 @@ function App() {
               </Tab>
             </Tabs>
 
-          <CardTemplate title={"Card 1"} />
-          <CardTemplate title={"Card 2"}/>
-          <CardTemplate title={"Card 3"}/>
+
         </PageContent>
       </Main>
     </Grommet>
--- src/components/SampleCard.tsx
+++ src/components/SampleCard.tsx
@@ -1,6 +1,43 @@
+import { Card, CardBody, CardFooter, CardHeader, Heading, Paragraph, ResponsiveContext } from "grommet";
+import { useContext } from "react";
+
+
+// Notes on the typescript error:
+// For an object, you need to declare the type as:
+// {text} : {text:any}
+// For objects with more props:
+// {a,b} : {a:any, b:any}
+const CardTemplate = ({ title } : { title:any }) => {
+    const size = useContext(ResponsiveContext);
+  
+    return (
+      <Card>
+        <CardHeader pad="medium">
+          <Heading level={2} margin="none">
+            {title}
+          </Heading>
+        </CardHeader>
+        <CardBody pad="medium">
+          <Paragraph maxLines={size === "small" ? 3 : undefined}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            porttitor non nulla ac vehicula. Aliquam erat volutpat. Mauris auctor
+            faucibus est at mattis. Aliquam a enim ac nisi aliquam consectetur et
+            ac velit. Mauris ut imperdiet libero.
+          </Paragraph>
+        </CardBody>
+        <CardFooter pad="medium" background="background-contrast">
+          Footer
+        </CardFooter>
+      </Card>
+    );
+  };
+
 const SampleCard = () => {
     return (
         <div>
+            <CardTemplate title={"Card 1"} />
+            <CardTemplate title={"Card 2"}/>
+            <CardTemplate title={"Card 3"}/>
         </div>
     );
 }
