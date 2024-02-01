d76b06d9978e905bf6f0c831aac51267a3b5fe84
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-02-01 00:08:46 -0700

    Now showing both income and expenses on combined view. Need to remove form entry

--- src/components/CombinedPage.tsx
+++ src/components/CombinedPage.tsx
@@ -1,37 +1,50 @@
 import React from "react";
-import { Box, Tab, Tabs, Text } from "grommet";
+// import { Box, Tab, Tabs, Text } from "grommet";
+import IncomePage from "./IncomePage";
+import ExpensePage from "./ExpensePage";
 
 const CombinedPage: React.FC = () => {
   return (
     <div>
-        <Box pad="medium">
-            <Text alignSelf='center'>
-                This will show all expenses combined by months ordered by date. 
-            </Text>
-            <Tabs>
-                <Tab title="January" >
-                    <Box pad="medium">
-                        <Text alignSelf='center'>
-                            entry feed.... 
-                        </Text>
-                    </Box>
-                </Tab>
-                <Tab title="Feburary" >
-                    <Box pad="medium">
-                        <Text alignSelf='center'>
-                            entry feed.... 
-                        </Text>
-                    </Box>
-                </Tab>
-                <Tab title="March" >
-                    <Box pad="medium">
-                        <Text alignSelf='center'>
-                            entry feed.... 
-                        </Text>
-                    </Box>
-                </Tab>
-            </Tabs>
-        </Box>
+        {/* 
+            This is ideally what I want to happen once I figure out how to combine entries across types. 
+
+            <Box pad="medium">
+                <Text alignSelf='center'>
+                    This will show all expenses combined by months ordered by date. 
+                </Text>
+
+                
+                <Tabs>
+                    <Tab title="January" >
+                        <Box pad="medium">
+                            <Text alignSelf='center'>
+                                entry feed.... 
+                            </Text>
+                        </Box>
+                    </Tab>
+                    <Tab title="Feburary" >
+                        <Box pad="medium">
+                            <Text alignSelf='center'>
+                                entry feed.... 
+                            </Text>
+                        </Box>
+                    </Tab>
+                    <Tab title="March" >
+                        <Box pad="medium">
+                            <Text alignSelf='center'>
+                                entry feed.... 
+                            </Text>
+                        </Box>
+                    </Tab>
+                </Tabs> 
+
+            </Box>
+        */}
+
+      <h1>Combined Page</h1>
+      <IncomePage />
+      <ExpensePage />
     </div>
   );
 }
