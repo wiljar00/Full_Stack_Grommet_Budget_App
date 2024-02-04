595273598eb6fc14b2dc60e8fd4072eefdbf5e50
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-02-03 17:43:55 -0700

    Add first route

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 // TODO: Define API routes here
 
+router.get('/hello', (req, res) => {
+    res.json(
+        { 
+            message: 'Hello, world!' 
+        }
+    );
+});
+
 module.exports = router;
