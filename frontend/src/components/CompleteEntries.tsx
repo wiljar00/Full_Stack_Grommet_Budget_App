3631418176380205195d5717ebc2c95970bc95a9
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-02-07 21:20:17 -0700

    Update CompleteEntries.tsx

--- frontend/src/components/CompleteEntries.tsx
+++ frontend/src/components/CompleteEntries.tsx
@@ -1,8 +1,45 @@
+import { Box, Button, TextInput } from "grommet";
+import EntryCard from "./EntryCard";
+import { useState } from "react";
+
+
+interface Entry {
+    id: number;
+    amount: number;
+    description: string;
+    date: string;
+}
 
 const CompleteEntries = () => {
+    const [loading, setLoading] = useState(false);
+    const [entries, setEntries] = useState<Entry | null>(null);
+
+    const fetchEntries = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(`http://localhost:5000/api/entries`);
+            const data: Entry = await response.json();
+            setEntries(data);
+        } catch (error) {
+            console.error('Error fetching entry:', error);
+            setEntries(null);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <div>
-            test
+            <h1>Search Entries</h1>
+
+            <Box direction="row" gap="small" margin={{ bottom: 'medium' }}>
+                <Button label="Search by ID" onClick={fetchEntries} primary />
+            </Box>
+
+            {loading && <p>Loading...</p>}
+            {/* TODO: Show a card for every object */}
+
+            {/* {entries && <EntryCard entry={entries} />} */}
         </div>
     );
 }
